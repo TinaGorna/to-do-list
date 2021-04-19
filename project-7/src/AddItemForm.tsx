92a5e72a7b08232e2b0ca1dd00d4295c7184f439
Kristina Gorbunova <77728617+TinaGorna@users.noreply.github.com>
2021-04-19 12:18:41 +0300

    Material UI used

---
+++ project-7/src/AddItemForm.tsx
@@ -0,0 +1,47 @@
+import {IconButton, TextField} from "@material-ui/core";
+import React, {ChangeEvent, KeyboardEvent, useState} from "react";
+import {ControlPoint} from "@material-ui/icons";
+
+type AddItemFormPropsType = {
+    addItem: (title: string) => void
+}
+
+export function AddItemForm(props: AddItemFormPropsType) {
+
+    let [title, setTitle] = useState("")
+    let [error, setError] = useState<string | null>(null)
+
+    const addItem = () => {
+        if (title.trim() !== "") {
+            props.addItem(title);
+            setTitle("");
+        } else {
+            setError("Title is required");
+        }
+    }
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
+    }
+
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        setError(null);
+        if (e.charCode === 13) {
+            addItem();
+        }
+    }
+
+    return <div>
+        <TextField value={title}
+                   label={"Type value"}
+                   variant={"outlined"}
+                   onChange={onChangeHandler}
+                   onKeyPress={onKeyPressHandler}
+                   error={!!error}
+                   helperText={error}
+        />
+        <IconButton onClick={addItem} color={"primary"}>
+            <ControlPoint/>
+        </IconButton>
+    </div>
+}
